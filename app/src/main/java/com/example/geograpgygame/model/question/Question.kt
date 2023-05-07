16075c0a28aa36bb8c943de461675c976fe7215b
walid <walid.abdullah.7e5@itb.cat>
2023-05-07 10:09:19 +0200

    answered and correct update var, add fun toString(), equals(), and hashCode()

--- app/src/main/java/com/example/geograpgygame/model/question/Question.kt
+++ app/src/main/java/com/example/geograpgygame/model/question/Question.kt
@@ -3,8 +3,35 @@ package com.example.geograpgygame.model.question
 class Question(
     val text : String,
     val answer : String,
-    val answered : Boolean,
-    val correct : Boolean,
+    var answered : Boolean,
+    var correct : Boolean,
     val options : List<String>,
     val type: QuestionType
-)
+){
+    override fun toString(): String {
+        return "Question(text='$text', answer='$answer', answered=$answered, correct=$correct, options=$options, type=$type)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Question){
+            return false
+        }
+        if (this.type != other.type){
+            return false
+        }
+        if (this.answer != other.answer){
+            return false
+        }
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = text.hashCode()
+        result = 31 * result + answer.hashCode()
+        result = 31 * result + answered.hashCode()
+        result = 31 * result + correct.hashCode()
+        result = 31 * result + options.hashCode()
+        result = 31 * result + type.hashCode()
+        return result
+    }
+}
